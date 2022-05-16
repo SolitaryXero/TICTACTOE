a314afd693598390f83a9706b7354d81bc547a8c
SolitaryXero <103876121+SolitaryXero@users.noreply.github.com>
2022-05-16 22:34:22 +0500

    Add files via upload

---
+++ MAIN.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+#include<windows.h>
+#include<string.h>
+#include<chrono>
+#include<thread>
+
+#include"game.txt"
+
+
+using namespace std;
+
+main(){
+    startingPrompt();
+    delay(2);
+    game();
+};
